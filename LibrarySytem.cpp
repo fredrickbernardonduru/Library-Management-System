dbaac7207b265d2e55f8327d9319e7ef9f82dcdd
Fredrick Bernard Onduru <fredrickbernardonduru@gmail.com>
2025-01-27 00:07:50 +0300

    Fix syntax error in Member class and add main function stub

--- LibrarySytem.cpp
+++ LibrarySytem.cpp
@@ -5,16 +5,44 @@ using string;
 
 class Book {
     private:
-    string title;
-    string author;
-    string publisher;
-    int year;
+    string Title;
+    string Author;
+    string Publisher;
+    int Year;
+
+    Book(string title, string author, string publisher, int year){
+        Tittle = title;
+        Author = author;
+        Publisher = publisher;
+        Year = year;
+    }
+
+    void display() {
+     cout << "Title: " << Tittle << endl; 
+     cout << "Author: "<< Author << endl;
+     cout << "Publisher: "<< Publisher << endl;
+     cout<< "Year: " << Year << endl;
+    }
 };
 
 class Library {
     private:
-    Book books[100];
-    int count;
+    Book Books[100];
+    int Count;
+
+    Library(string books, int count){
+        Books = book;
+        Count = count;
+    }
+
+    void addBook(Book book){
+        Books[Count] = book;
+        Count++;
+    }
+
+
+
+    
     };
 
 class Member {
@@ -24,7 +52,10 @@ class Member {
     string phone;
     int id;
     int books[3];
+};
+
+int main(){
+
+    retun 0;
 }
-
-
 
